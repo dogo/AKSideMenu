dc75f87e0cdb442c9a5a31419e0931c0b9e39417
David Collado <bitomule@gmail.com>
2017-02-01 11:45:27 +0100

    Add project framework files

---
+++ AKSideMenu/AKSideMenu.h
@@ -0,0 +1,19 @@
+//
+//  AKSideMenu.h
+//  AKSideMenu
+//
+//  Created by David Collado on 1/2/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AKSideMenu.
+FOUNDATION_EXPORT double AKSideMenuVersionNumber;
+
+//! Project version string for AKSideMenu.
+FOUNDATION_EXPORT const unsigned char AKSideMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AKSideMenu/PublicHeader.h>
+
+
