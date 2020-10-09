b2246124cd66df90481d537666977c787fe1f6bd
Diogo Autilio <diautilio@gmail.com>
2020-10-09 14:28:42 -0300

    Add github action

---
+++ .github/scripts/build_app.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [ "$1" = "simple" ]
+then
+	xcodebuild -workspace AKSideMenuExamples/Simple/AKSideMenuSimple.xcworkspace \
+	            -scheme AKSideMenuSimple \
+	            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
+	            clean build | xcpretty
+elif [ "$2" = "storyboard" ]
+then
+	xcodebuild -workspace AKSideMenuExamples/Storyboard/AKSideMenuStoryboard.xcworkspace \
+	            -scheme AKSideMenuStoryboard \
+	            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
+	            clean build | xcpretty 
+fi           
