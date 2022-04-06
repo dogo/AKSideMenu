fdbbd8aa51e5e190bead4f79e4aaa7a23f1a75fd
Diogo Autilio <diautilio@gmail.com>
2022-04-05 21:06:11 -0300

    Update build_app.sh

--- .github/scripts/build_app.sh
+++ .github/scripts/build_app.sh
@@ -6,12 +6,12 @@ if [ "$1" = "simple" ]
 then
 	xcodebuild -workspace AKSideMenuExamples/Simple/AKSideMenuSimple.xcworkspace \
 	            -scheme AKSideMenuSimple \
-	            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
+	            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
 	            clean build | xcpretty
 elif [ "$2" = "storyboard" ]
 then
 	xcodebuild -workspace AKSideMenuExamples/Storyboard/AKSideMenuStoryboard.xcworkspace \
 	            -scheme AKSideMenuStoryboard \
-	            -destination platform=iOS\ Simulator,OS=14.0,name=iPhone\ 11 \
+	            -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 11 \
 	            clean build | xcpretty 
 fi           
