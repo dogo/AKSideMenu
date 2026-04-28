a4a784a88b01c8e7ffe59c8ca6b976884311a1b8
Diogo Autilio <diautilio@gmail.com>
2026-04-28 19:14:02 -0300

    Add side menu lifecycle tests

--- Tests/AKSideMenuTests/AKSideMenuTests.swift
+++ Tests/AKSideMenuTests/AKSideMenuTests.swift
@@ -1,15 +1,55 @@
 @testable import AKSideMenu
+import UIKit
 import XCTest
 
 final class AKSideMenuTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(AKSideMenu().text, "Hello, World!")
+    func testSideMenuViewControllerFindsAncestor() {
+        let rootViewController = UIViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        let sideMenu = AKSideMenu(contentViewController: navigationController,
+                                  leftMenuViewController: UIViewController(),
+                                  rightMenuViewController: nil)
+
+        sideMenu.loadViewIfNeeded()
+
+        XCTAssertIdentical(rootViewController.sideMenuViewController, sideMenu)
+    }
+
+    func testSetContentViewControllerWithoutAnimationReplacesChildController() {
+        let initialContentViewController = UIViewController()
+        let sideMenu = AKSideMenu(contentViewController: initialContentViewController,
+                                  leftMenuViewController: nil,
+                                  rightMenuViewController: nil)
+        sideMenu.loadViewIfNeeded()
+
+        let replacementViewController = UIViewController()
+        sideMenu.setContentViewController(replacementViewController, animated: false)
+
+        XCTAssertNil(initialContentViewController.parent)
+        XCTAssertIdentical(replacementViewController.parent, sideMenu)
+        XCTAssertIdentical(sideMenu.contentViewController, replacementViewController)
+        XCTAssertNotNil(replacementViewController.view.superview)
+    }
+
+    func testReplacingMenuViewControllerUpdatesChildController() {
+        let initialMenuViewController = UIViewController()
+        let sideMenu = AKSideMenu(contentViewController: UIViewController(),
+                                  leftMenuViewController: initialMenuViewController,
+                                  rightMenuViewController: nil)
+        sideMenu.loadViewIfNeeded()
+
+        let replacementMenuViewController = UIViewController()
+        sideMenu.leftMenuViewController = replacementMenuViewController
+
+        XCTAssertNil(initialMenuViewController.parent)
+        XCTAssertIdentical(replacementMenuViewController.parent, sideMenu)
+        XCTAssertIdentical(sideMenu.leftMenuViewController, replacementMenuViewController)
+        XCTAssertNotNil(replacementMenuViewController.view.superview)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testSideMenuViewControllerFindsAncestor", testSideMenuViewControllerFindsAncestor),
+        ("testSetContentViewControllerWithoutAnimationReplacesChildController", testSetContentViewControllerWithoutAnimationReplacesChildController),
+        ("testReplacingMenuViewControllerUpdatesChildController", testReplacingMenuViewControllerUpdatesChildController),
     ]
 }
